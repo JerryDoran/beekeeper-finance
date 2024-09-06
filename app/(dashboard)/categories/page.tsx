cd94e64d99710543129474171353d5f8a682ab7e
Jerome Doran <disneyfan9184@gmail.com>
2024-09-06 16:39:57 -0400

    created categories api and categories ui

--- app/(dashboard)/categories/page.tsx
+++ app/(dashboard)/categories/page.tsx
@@ -11,7 +11,7 @@ import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { DataTable } from '@/components/data-table';
 import { Skeleton } from '@/components/ui/skeleton';
 
-export default function AccountsPage() {
+export default function CategoriesPage() {
   const { onOpen } = useNewAccount();
   const deleteAccounts = useBulkDeleteAccounts();
   const accountsQuery = useGetAccounts();
@@ -40,7 +40,9 @@ export default function AccountsPage() {
     <div className='max-w-screen-2xl mx-auto w-full pb-10 -mt-24'>
       <Card className='border-none drop-shadow-sm dark:shadow-gray-700'>
         <CardHeader className='gap-y-2 lg:flex-row lg:items-center lg:justify-between'>
-          <CardTitle className='text-xl line-clamp-1'>Accounts Page</CardTitle>
+          <CardTitle className='text-xl line-clamp-1'>
+            Categories Page
+          </CardTitle>
           <Button size='sm' onClick={onOpen}>
             <Plus className='size-4 mr-2' />
             Add new
